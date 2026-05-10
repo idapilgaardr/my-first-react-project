fe01ca9750e7b84cdeaeafe808cb78aa1d1aa116
idapr <idap.rasmussen@gmail.com>
2026-05-10 19:03:12 +0200

    Update User.jsx

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,7 +2,7 @@ export default function User({ name, mail, image }) {
   return (
     <div className="user-card">
       <img src={image} alt={name} />
-      <h2>Hello, I'm {name} 👋🏻</h2>
+      <h2>Hello, I'm {name}</h2>
       <p>Contact me on my email {mail}</p>
     </div>
   );
